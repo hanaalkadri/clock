39987fde1300a8f28091cd4f1509636ae491d309
hanaalkadri <hanaalkadry@gmail.com>
2025-04-10 15:26:39 +0200

    added the css style

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-let parisTime = moment.tz("Europe/Paris").format("dddd Do MMMM H:mm");
-let sydneyTime = moment.tz("Australia/Sydney").format("dddd Do MMMM H:mm");
-let tokyoTime = moment.tz("Asia/Tokyo").format("dddd Do MMMM H:mm");
+// let parisTime = moment.tz("Europe/Paris").format("dddd Do MMMM H:mm");
+// let sydneyTime = moment.tz("Australia/Sydney").format("dddd Do MMMM H:mm");
+// let tokyoTime = moment.tz("Asia/Tokyo").format("dddd Do MMMM H:mm");
 
-function ShowCityTime(event) {
-  const city = event.target.value;
+// function ShowCityTime(event) {
+//   const city = event.target.value;
 
-  if (!city) {
-    alert("Please select a city");
-    return;
-  }
+//   if (!city) {
+//     alert("Please select a city");
+//     return;
+//   }
 
-  if (city === "Paris") {
-    alert(parisTime);
-  } else if (city === "Sydney") {
-    alert(sydneyTime);
-  } else if (city === "Tokyo") {
-    alert(tokyoTime);
-  }
-}
+//   if (city === "Paris") {
+//     alert(parisTime);
+//   } else if (city === "Sydney") {
+//     alert(sydneyTime);
+//   } else if (city === "Tokyo") {
+//     alert(tokyoTime);
+//   }
+// }
 
-let citySelect = document.querySelector("#city");
-citySelect.addEventListener("change", ShowCityTime);
+// let citySelect = document.querySelector("#city");
+// citySelect.addEventListener("change", ShowCityTime);
