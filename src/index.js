16215722756470f27c21d0db49032769fdca73d0
hanaalkadri <hanaalkadry@gmail.com>
2025-04-10 16:54:35 +0200

    add the basic javascript

--- src/index.js
+++ src/index.js
@@ -1,23 +1,22 @@
-// let parisTime = moment.tz("Europe/Paris").format("dddd Do MMMM H:mm");
-// let sydneyTime = moment.tz("Australia/Sydney").format("dddd Do MMMM H:mm");
-// let tokyoTime = moment.tz("Asia/Tokyo").format("dddd Do MMMM H:mm");
+function updateTime() {
+  //los angeles
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
 
-// function ShowCityTime(event) {
-//   const city = event.target.value;
-
-//   if (!city) {
-//     alert("Please select a city");
-//     return;
-//   }
-
-//   if (city === "Paris") {
-//     alert(parisTime);
-//   } else if (city === "Sydney") {
-//     alert(sydneyTime);
-//   } else if (city === "Tokyo") {
-//     alert(tokyoTime);
-//   }
-// }
-
-// let citySelect = document.querySelector("#city");
-// citySelect.addEventListener("change", ShowCityTime);
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = `${losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+  let parisTime = moment.tz("Europe/Paris");
+  let parisElement = document.querySelector("#paris");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisDateElement = parisElement.querySelector(".date");
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = `${parisTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  )}`;
+}
+updateTime();
+setInterval(updateTime, 1000);
