a146d66668e7c28fda6006e0a117bcab253cbf27
hanaalkadri <hanaalkadry@gmail.com>
2025-04-10 10:48:25 +0200

    create a js file

---
+++ src/index.js
@@ -0,0 +1,23 @@
+function ShowCityTime(event) {
+  const city = event.target.value;
+
+  if (!city) {
+    alert("Please select a city");
+    return;
+  }
+
+  if (city === "Paris") {
+    alert(parisTime);
+  } else if (city === "Sydney") {
+    alert(sydneyTime);
+  } else if (city === "Tokyo") {
+    alert(tokyoTime);
+  }
+}
+
+let citySelect = document.querySelector("#city");
+citySelect.addEventListener("change", ShowCityTime);
+
+let parisTime = moment.tz("Europe/Paris").format("dddd Do MMMM H:mm");
+let sydneyTime = moment.tz("Australia/Sydney").format("dddd Do MMMM H:mm");
+let tokyoTime = moment.tz("Asia/Tokyo").format("dddd Do MMMM H:mm");
